5c08bf8aeb277c80c9a43569bc5539986b4cedfc
Nina <nina.novoselska@gmail.com>
2018-01-11 20:42:37 +0200

    implement area calculation in ComplexShape

--- src/main/java/co/inventorsoft/oop/task/ApplicationStarter.java
+++ src/main/java/co/inventorsoft/oop/task/ApplicationStarter.java
@@ -1,10 +1,7 @@
 package co.inventorsoft.oop.task;
 
 import co.inventorsoft.oop.task.area.ComplexShape;
-import co.inventorsoft.oop.task.shapes.Circle;
-import co.inventorsoft.oop.task.shapes.Rectangle;
-import co.inventorsoft.oop.task.shapes.Square;
-import co.inventorsoft.oop.task.shapes.Triangle;
+import co.inventorsoft.oop.task.shapes.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +9,11 @@ import java.util.List;
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-        final List<Object> shapes = Arrays.asList(new Circle(), new Square(), new Rectangle(), new Triangle());
+        final List<Shape> shapes = Arrays.asList(
+                new Circle(3),
+                new Square(2),
+                new Rectangle(2,3),
+                new Triangle(2,3));
         final ComplexShape complexShape = new ComplexShape(shapes);
         System.out.printf("Complex shape area = %f", complexShape.area());
     }
--- src/main/java/co/inventorsoft/oop/task/area/ComplexShape.java
+++ src/main/java/co/inventorsoft/oop/task/area/ComplexShape.java
@@ -1,18 +1,24 @@
 package co.inventorsoft.oop.task.area;
 
+
+import co.inventorsoft.oop.task.shapes.Shape;
+
 import java.util.List;
 
 // TODO: 1/10/2018 Implement area calculation for underlying shapes
 public class ComplexShape {
 
-    private List<> shapes;
+    private List<Shape> shapes;
 
-    public ComplexShape(List<Object> shapes) {
+    public ComplexShape(List<Shape> shapes) {
         this.shapes = shapes;
     }
-
-
+    
     public double area() {
-
+        double area = 0;
+        for (Shape shape: shapes) {
+            area += shape.area();
+        }
+        return area;
     }
 }
