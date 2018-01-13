d609c984d54a6d936c39be3a3087c134570d6c29
maksym.studenets@gmail.com <Maksym10081997fkn>
2018-01-13 17:01:03 +0200

    Fixed constructors

--- src/main/java/co/inventorsoft/oop/task/ApplicationStarter.java
+++ src/main/java/co/inventorsoft/oop/task/ApplicationStarter.java
@@ -9,7 +9,11 @@ import java.util.List;
 public class ApplicationStarter {
 
     public static void main(String[] args) {
-        final List<Shape> shapes = Arrays.asList(new Circle(), new Square(), new Rectangle(), new Triangle());
+        final List<Shape> shapes = Arrays.asList(
+                new Circle(5.47),
+                new Square(7.2),
+                new Rectangle(2.85, 5.89),
+                new Triangle(3.48, 2.26, 4.18));
         final ComplexShape complexShape = new ComplexShape(shapes);
         System.out.printf("Complex shape area = %f", complexShape.area());
     }
